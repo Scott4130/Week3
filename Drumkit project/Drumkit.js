18faa408711f4b5a37afb8b9350d3aa9c38ecb21
scott4130 <scott4130@outlook.com>
2021-12-01 12:01:47 +0000

    drums and keycode done

---
+++ Drumkit project/Drumkit.js
@@ -0,0 +1,42 @@
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 81) {
+      document.getElementById('q').play();
+    }
+  });
+
+
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('a').play();
+    }
+  });
+
+
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 90) {
+      document.getElementById('z').play();
+    }
+  });
+
+
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 80) {
+      document.getElementById('p').play();
+    }
+  });
+
+
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 76) {
+      document.getElementById('l').play();
+    }
+  });
+
+
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 77) {
+      document.getElementById('m').play();
+    }
+  });
+
+
